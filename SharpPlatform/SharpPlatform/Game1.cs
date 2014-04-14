004e30cd3df73b20121812065f06b8f51e41f158
Ravencall <andreaskobberoe@hotmail.com>
2014-04-14 17:56:23 +0200

    Hejmeddig

--- SharpPlatform/SharpPlatform/Game1.cs
+++ SharpPlatform/SharpPlatform/Game1.cs
@@ -29,7 +29,8 @@ namespace SharpPlatform
 		public Rectangle playerRec, enemyRec, ground;
 		float gravity = 0.1f;
 		bool patrick = false;
-		
+		float pik;
+
 		bool jumping;
 		float startY, jumpspeed = 0.0f;
 
