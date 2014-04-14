aa746508e6f3bda40868225c3becb6ecde16d7b4
FieM <great_dragon_lover@hotmail.com>
2014-04-14 17:35:00 +0200

    THIS WORKS - starting over
    finally starting over

--- SharpPlatform/SharpPlatform/Game1.cs
+++ SharpPlatform/SharpPlatform/Game1.cs
@@ -19,12 +19,24 @@ namespace SharpPlatform
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
-		private Texture2D sprite;
-		private Rectangle chara;
-		int gravity = 0;
+
+		Texture2D playerSprite, enemySprite, groundSprite;
+		public Vector2 player, enemy = new Vector2(100,100);
+		KeyboardState keystate;
+		Color playerColor = Color.White;
+		Color enemyColor = Color.Black;
+		float moveSpeed = 500f;
+		public Rectangle playerRec, enemyRec, ground;
+		float gravity = 0.1f;
 
 		bool jumping;
-		int startY, jumpspeed = 0;
+		float startY, jumpspeed = 0.0f;
+
+		Camera camera;
+
+		// Background
+		Texture2D backgroundTexture;
+		Vector2 backgroundPosition;
 
 		public Game1 ()
 		{
@@ -41,9 +53,16 @@ namespace SharpPlatform
 		/// </summary>
 		protected override void Initialize ()
 		{
+			camera = new Camera(GraphicsDevice.Viewport);
 			// TODO: Add your initialization logic here
 			base.Initialize (); // Calls LoadContent, and therefore gets the width and height of enemy and player
+			enemyRec = new Rectangle ((int)enemy.X, (int)enemy.Y, enemySprite.Width, enemySprite.Height);
+			playerRec = new Rectangle ((int)player.X, (int)player.Y, playerSprite.Width, playerSprite.Height);
+			ground = new Rectangle (-50, 300, 300, 50);
 
+			startY = player.Y;
+			jumping = false;
+			jumpspeed = 0;
 		}
 
 		/// <summary>
@@ -54,32 +73,16 @@ namespace SharpPlatform
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch (GraphicsDevice);
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-			//TODO: use this.Content to load your game content here 
-			sprite = Content.Load<Texture2D> ("vehiconava");
-			chara = new Rectangle (0, 0, 100, 100);
-			startY = chara.Y;
-			jumping = false;
-			jumpspeed = 0;
-=======
-=======
->>>>>>> origin/Fie
-			//TODO: use this.Content to load your game content here
-=======
 			//TODO: use this.Content to load your game content here 
 			playerSprite = Content.Load<Texture2D> ("hero");
 			enemySprite = Content.Load<Texture2D> ("hero");
 			groundSprite = Content.Load<Texture2D> ("ground");
-			
+
 			backgroundTexture = Content.Load<Texture2D> ("Background");
 			backgroundPosition = new Vector2 (-400, 0);
 
->>>>>>> parent of 8cbd8af... Creating Classes
 
->>>>>>> 8cbd8afa785ac1a46e5e7d73ca157c453035153f
 		}
 
 		/// <summary>
@@ -89,14 +92,10 @@ namespace SharpPlatform
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update (GameTime gameTime)
 		{
-<<<<<<< HEAD
-			KeyboardState KeyBS = Keyboard.GetState ();
 			// For Mobile devices, this logic will close the Game when the Back button is pressed
 			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || keystate.IsKeyDown(Keys.Escape) || keystate.IsKeyDown (Keys.Back)) {
 				Exit ();
 			}
-<<<<<<< HEAD
-=======
 			// TODO: Add your update logic here			
 			keystate = Keyboard.GetState ();
 
@@ -135,44 +134,53 @@ namespace SharpPlatform
 			} else if (keystate.IsKeyDown (Keys.S)) {
 				enemy.Y += moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 			}
->>>>>>> parent of 8cbd8af... Creating Classes
 
-			if (KeyBS.IsKeyDown (Keys.Right))
-				chara.X += 5;
-			if (KeyBS.IsKeyDown (Keys.Left))
-				chara.X -= 5;
-			if (KeyBS.IsKeyDown (Keys.Down))
-				chara.Y += 5;
-			if (KeyBS.IsKeyDown (Keys.Up))
-				chara.Y -= 5;
+			//Adjust enemyRec, so that it follows enemy
+			enemyRec.X = (int)enemy.X;
+			enemyRec.Y = (int)enemy.Y;
 
-			chara.Y += gravity;
-			gravity += 1;
-			if (gravity > 2)
-				gravity = 2;
+			//Collision checking - NEW METHOD
+			if (playerRec.Intersects(enemyRec)) {
+				//collision
+				playerColor = Color.Red;
+			} else {
+				//No collision
+				playerColor = Color.White;
+			}
 
 			if (jumping) {
-				chara.Y += jumpspeed;
+				player.Y += jumpspeed;
 				jumpspeed += 1;
-				if (chara.Y >= startY) {
-					chara.Y = startY;
+				if (player.Y > player.Y + 5) {
 					jumping = false;
 				}
 			} 
 			else {
-				if (KeyBS.IsKeyDown (Keys.Space)) {
+				if (keystate.IsKeyDown (Keys.Space)) {
 					jumping = true;
-					jumpspeed = -14;
+					jumpspeed = -50;
 				}
 			}
 
-
-=======
->>>>>>> origin/Fie
-			// TODO: Add your update logic here			
-
+			//collision ();
+			camera.Update (gameTime, this);
 			base.Update (gameTime);
 		}
+		/*OLD COLLISION METHOD	
+		private void  collision()
+			{
+		
+			if (playerPos.X + player.Width < enemyPos.X || playerPos.X > enemy.Width + enemyPos.X ||
+				playerPos.Y + player.Width < enemyPos.Y || playerPos.Y > enemy.Width + enemyPos.Y) {
+				//No collision, checking that player is not in the position that the enemy is
+				playerColor = Color.White;
+			} else {
+				//Collision between player and enemy
+				playerColor = Color.Red;
+			}
+		}
+		*/
+
 		/// <summary>
 		/// This is called when the game should draw itself.
 		/// </summary>
@@ -180,14 +188,8 @@ namespace SharpPlatform
 		protected override void Draw (GameTime gameTime)
 		{
 			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
-		
+
 			//TODO: Add your drawing code here
-<<<<<<< HEAD
-<<<<<<< HEAD
-            
-			spriteBatch.Begin ();
-			spriteBatch.Draw (sprite, chara, Color.White);
-=======
 			spriteBatch.Begin (SpriteSortMode.Deferred, 
 				BlendState.AlphaBlend, 
 				null, null, null, null,
@@ -196,12 +198,8 @@ namespace SharpPlatform
 			spriteBatch.Draw (playerSprite, player, playerColor);
 			spriteBatch.Draw (enemySprite, enemy, enemyColor);
 			spriteBatch.Draw (groundSprite, ground, Color.White);
->>>>>>> 8cbd8afa785ac1a46e5e7d73ca157c453035153f
 			spriteBatch.End ();
 
-=======
-	
->>>>>>> origin/Fie
 			base.Draw (gameTime);
 		}
 	}
